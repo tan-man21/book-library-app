c90508e6ed35b9b07ef48294cd176f2918344979
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-02-27 17:37:48 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchColumn from './components/SearchColumn';
+import BookColumn from './components/BookColumn';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col><SearchColumn /></Col>
+          <Col><BookColumn /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/BookColumn.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+
+function BookColumn() {
+
+    return (
+        <div className='book-column'>
+            <h2>Book Data</h2>
+        </div>
+    )
+}
+
+export default BookColumn
